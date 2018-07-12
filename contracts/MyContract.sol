f36675c4b730c5d01c5e61a0aca0614786b1ac28
Ling Qing Meng <ling.q.meng@gmail.com>
2018-07-12 17:15:04 +0900

    updating syntax to 4.21 and change signature to void return type

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -1,20 +1,21 @@
-pragma solidity ^0.4.18; 
+pragma solidity ^0.4.21; 
 
 contract MyContract {
 
-    uint public mynumber; 
-    /* Modified simple storage function. Only stores numbers greater than 10. */
-    
-    function MyContract() {
-        mynumber = 24; 
-    }
+  uint public mynumber; 
+  /* Modified simple storage function. Only stores numbers greater than 10. */
+  
+  constructor() 
+    public 
+  {
+    mynumber = 24; 
+  }
 
-    function storeNum(uint mynum)
-        public
-        returns (bool success)
-    {
-     require(mynum > 10);
-     mynumber = mynum; 
-     return true;    
-    }
+  function storeNum(uint mynum)
+    public
+  {
+    require(mynum > 10);
+    mynumber = mynum; 
+   
+  }
 }
